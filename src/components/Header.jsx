3bfe4005865fa665524f914d9a2b740b21a80a6b
joaopedrosduarte <jpds@cesar.school>
2023-10-24 15:22:18 -0300

    Changing the icon's lib

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
-import { List } from 'phosphor-react';
-
+import { X, List } from 'phosphor-react';
 
 export default function Header() {
     const links = [
@@ -12,20 +11,21 @@ export default function Header() {
     const [open, setOpen] = useState(false);
 
     return (
-        <header className='bg-[#27272c] shadow-md p-6'>
+        <header className='bg-[#27272c] shadow-lg py-6 px-10'>
             <nav className="flex justify-between">
                 <h1>
                     <a href="/">Jpds.dev</a>
                 </h1>
-                <ul className={`md:flex md:flex-row flex-col flex justify-center align-middle md:gap-6 md:static md:h-auto md:w-auto absolute h-screen w-full bg-[#27272c] left-0 top-0 ${open ? "" : "hidden"} `}>
+                <ul className={`float-right transition-all duration-300 md:flex md:flex-row flex-col flex justify-center align-middle md:gap-6 md:static md:h-auto md:w-auto absolute h-screen w-full bg-[#27272c] left-0 top-0 ${open ? "" : "left-[-100%]"}`}>
                     {links.map((link) => (
-                        <li className='flex flex-1 justify-center items-center h-max'><a href={link.link}>{link.name}</a></li>
+                        <a className='flex flex-1 justify-center items-center h-max duration-300 hover:bg-zinc-700' href={link.link}>{link.name}</a>
                     ))}
+                    <div onClick={() => setOpen(!open)} className='w-9 md:absolute md:hidden absolute right-8 top-6 cursor-pointer'>
+                        <X size={32} color="rgb(255, 255, 255)" weight="bold" />
+                    </div>
                 </ul>
-                <div onClick={() => setOpen(!open)} className="w-9 absolute right-8 top-6 cursor-pointer md:hidden">
-                    <span className="text-3xl text-white">
-                        <ion-icon name={open ? "close" : "menu"}></ion-icon>
-                    </span>
+                <div onClick={() => setOpen(!open)} className={`static md:hidden cursor-pointer`}>
+                    <List size={32} color="rgb(255, 255, 255)" weight="bold" />
                 </div>
             </nav>
         </header >
