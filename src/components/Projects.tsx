fe02daf7680897dea2e48fdf425c683b48e70c6a
joaopedrosduarte <jpds@cesar.school>
2024-02-22 01:24:19 -0300

    add conditional case to empty response from the database to projects section (using a empty array to interate while the db is not ready)

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,35 +1,45 @@
 import Project from "./Project";
 
 export default function Projects() {
+  const projects = [];
   return (
     <div className="flex w-full justify-center items-center">
       <div className="flex w-full max-w-6xl flex-col px-10 py-24 gap-8 align-middle">
-        {/* FAZER UM CONDICIONAL RELACIONADO COM O BANCO */}
-        <div className="flex flex-col gap-2 mb4:text-left text-center">
-          <h1 className="uppercase text-blue-500 font-bold text-base">
-            Projects
-          </h1>
-          <span className="font-semibold text-2xl pb-4 text-darkmode-maintext">
-            Cada projeto que fez parte da minha caminhada, até agora. 😉
-          </span>
-        </div>
-        {/* TODO: iterar banco com os projetos */}
-        <div className="flex flex-col gap-16 items-center">
-          <Project
-            ghLink="#"
-            projectDescription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae eaque debitis error illo quia quisquam accusamus. Iusto impedit quis libero, veritatis non exercitationem soluta eaque eius! Magnam neque odio earum?"
-            projectImage="#"
-            projectLink="miau"
-            projectTitle="Projeto 1"
-          />
-          <Project
-            ghLink="#"
-            projectDescription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae eaque debitis error illo quia quisquam accusamus. Iusto impedit quis libero, veritatis non exercitationem soluta eaque eius! "
-            projectImage="#"
-            projectLink="miau"
-            projectTitle="Projeto 2"
-          />
-        </div>
+        {projects.length === 0 ? (
+          <div className="flex flex-col gap-2 mb4:text-center text-center">
+            <span className="font-semibold text-2xl text-darkmode-maintext">
+              Os Projetos estão sendo atualizados e em breve estarão disponíveis! 😁
+            </span>
+          </div>
+        ) : (
+          <>
+            {/* FAZER UM MAP RELACIONADO COM O BANCO */}
+            <div className="flex flex-col gap-2 mb4:text-left text-center">
+              <h1 className="uppercase text-blue-500 font-bold text-base">
+                Projects
+              </h1>
+              <span className="font-semibold text-2xl pb-4 text-darkmode-maintext">
+                Cada projeto que fez parte da minha caminhada, até agora. 😉
+              </span>
+            </div>
+            <div className="flex flex-col gap-16 items-center">
+              <Project
+                ghLink="#"
+                projectDescription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae eaque debitis error illo quia quisquam accusamus. Iusto impedit quis libero, veritatis non exercitationem soluta eaque eius! Magnam neque odio earum?"
+                projectImage="#"
+                projectLink="miau"
+                projectTitle="Projeto 1"
+              />
+              <Project
+                ghLink="#"
+                projectDescription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae eaque debitis error illo quia quisquam accusamus. Iusto impedit quis libero, veritatis non exercitationem soluta eaque eius! "
+                projectImage="#"
+                projectLink="miau"
+                projectTitle="Projeto 2"
+              />
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
