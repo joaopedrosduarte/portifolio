bec43333d05a42a432f11e5c141999882e66e6fd
joaopedrosduarte <jpds@cesar.school>
2024-02-22 18:28:35 -0300

    adjust hero section responsivity, change the background of the techicons and delete 2 ways of contact in the contact section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,35 +1,50 @@
 import { DownloadSimple, GithubLogo, LinkedinLogo } from "phosphor-react";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { blue } from "tailwindcss/colors";
 
 export default function Hero() {
-
   return (
     <div className="flex w-full justify-center items-center">
-      <div className="flex flex-col max-w-5xl w-full items-center px-10 pt-60 pb-40">
-        <div className="w-full gap-10 flex flex-col justify-between items-center">
-          <span className="flex flex-col gap-3">
+      <div className="flex flex-col max-w-5xl w-full items-center px-10 pt-72 pb-48">
+        <div className="w-full gap-16 flex flex-col justify-between items-center">
+          <span className="flex flex-col gap-1">
             <span className="text-4xl text-darkmode-auxiliartext leading-[4rem] font-semibold">
-              Eae tudo certo? 👋🏼 {" "}
+              Eae tudo certo? 👋🏼{" "}
             </span>
-            <span className="text-3xl text-darkmode-maintext leading-[3.2rem] font-semibold">
-              Sou João Pedro Duarte, um brasileiro 🇧🇷 entusiasta da tecnologia desde pequeno 🚀 e na
-              procura de ingressar no mercado como desenvolvedor Full-Stack. 👨‍💻
+            <span className="text-3xl text-darkmode-maintext leading-[3rem] font-semibold">
+              Sou João Pedro Duarte, um brasileiro 🇧🇷 entusiasta da tecnologia
+              desde pequeno 🚀 e na procura de ingressar no mercado como
+              desenvolvedor Full-Stack. 👨‍💻
             </span>
           </span>
-          <div className="w-full flex justify-between align-middle">
-            <button className="px-4 py-3 bg-blue-600 duration-300 transition-shadow hover:shadow-blue-600 hover:shadow-[0px_2px_40px_-4px] rounded-lg flex gap-2 items-center">
-              <p className="font-normal text-base text-darkmode-maintext">Resume.pdf</p>
-              <DownloadSimple size={20} weight="bold" color="#E8E8FD" />
+          <div className="w-full flex mb3:flex-row flex-col gap-5 justify-between align-middle">
+            <button className="px-4 py-3 justify-center items-center bg-blue-600 duration-300 transition-shadow hover:shadow-blue-600 hover:shadow-[0px_2px_40px_-4px] rounded-lg flex gap-2">
+              <p className="font-medium mb4:text-base text-sm text-darkmode-maintext">
+                Resume.pdf
+              </p>
+              <DownloadSimple
+                size={700 <= 672 ? 18 : 20}
+                weight="bold"
+                color="#E8E8FD"
+              />
             </button>
-            <div className="flex gap-8">
+            <div className="flex gap-4 mb6:gap-8 justify-evenly">
               <div className="flex gap-1 items-center cursor-pointer transition-colors duration-300 text-blue-500 hover:text-zinc-200">
-                <LinkedinLogo size={24} />
-                <a className="text-lg font-medium">Linkedin</a>
+                <LinkedinLogo size={26} />
+                <a className="mb4:text-lg font-medium text-base">Linkedin</a>
               </div>
               <div className="flex gap-1 items-center cursor-pointer transition-colors duration-300 fill-blue-500 hover:fill-zinc-200 text-blue-500 hover:text-zinc-200">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 256 256"><path d="M208.31,75.68A59.78,59.78,0,0,0,202.93,28,8,8,0,0,0,196,24a59.75,59.75,0,0,0-48,24H124A59.75,59.75,0,0,0,76,24a8,8,0,0,0-6.93,4,59.78,59.78,0,0,0-5.38,47.68A58.14,58.14,0,0,0,56,104v8a56.06,56.06,0,0,0,48.44,55.47A39.8,39.8,0,0,0,96,192v8H72a24,24,0,0,1-24-24A40,40,0,0,0,8,136a8,8,0,0,0,0,16,24,24,0,0,1,24,24,40,40,0,0,0,40,40H96v16a8,8,0,0,0,16,0V192a24,24,0,0,1,48,0v40a8,8,0,0,0,16,0V192a39.8,39.8,0,0,0-8.44-24.53A56.06,56.06,0,0,0,216,112v-8A58.14,58.14,0,0,0,208.31,75.68ZM200,112a40,40,0,0,1-40,40H112a40,40,0,0,1-40-40v-8a41.74,41.74,0,0,1,6.9-22.48A8,8,0,0,0,80,73.83a43.81,43.81,0,0,1,.79-33.58,43.88,43.88,0,0,1,32.32,20.06A8,8,0,0,0,119.82,64h32.35a8,8,0,0,0,6.74-3.69,43.87,43.87,0,0,1,32.32-20.06A43.81,43.81,0,0,1,192,73.83a8.09,8.09,0,0,0,1,7.65A41.72,41.72,0,0,1,200,104Z"></path></svg>
-                <a className="text-lg font-medium">@joaopedrosduarte</a>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 256 256"
+                >
+                  <path d="M208.31,75.68A59.78,59.78,0,0,0,202.93,28,8,8,0,0,0,196,24a59.75,59.75,0,0,0-48,24H124A59.75,59.75,0,0,0,76,24a8,8,0,0,0-6.93,4,59.78,59.78,0,0,0-5.38,47.68A58.14,58.14,0,0,0,56,104v8a56.06,56.06,0,0,0,48.44,55.47A39.8,39.8,0,0,0,96,192v8H72a24,24,0,0,1-24-24A40,40,0,0,0,8,136a8,8,0,0,0,0,16,24,24,0,0,1,24,24,40,40,0,0,0,40,40H96v16a8,8,0,0,0,16,0V192a24,24,0,0,1,48,0v40a8,8,0,0,0,16,0V192a39.8,39.8,0,0,0-8.44-24.53A56.06,56.06,0,0,0,216,112v-8A58.14,58.14,0,0,0,208.31,75.68ZM200,112a40,40,0,0,1-40,40H112a40,40,0,0,1-40-40v-8a41.74,41.74,0,0,1,6.9-22.48A8,8,0,0,0,80,73.83a43.81,43.81,0,0,1,.79-33.58,43.88,43.88,0,0,1,32.32,20.06A8,8,0,0,0,119.82,64h32.35a8,8,0,0,0,6.74-3.69,43.87,43.87,0,0,1,32.32-20.06A43.81,43.81,0,0,1,192,73.83a8.09,8.09,0,0,0,1,7.65A41.72,41.72,0,0,1,200,104Z"></path>
+                </svg>
+                <a className="mb4:text-lg font-medium text-base">
+                  @joaopedrosduarte
+                </a>
               </div>
             </div>
           </div>
--- src/components/TechIcons.tsx
+++ src/components/TechIcons.tsx
@@ -10,13 +10,13 @@ interface TechIconsProps {
 export default function TechIcons({tech}:TechIconsProps) {
 	const [hover, setHover] = useState(false);
 	const controls = useAnimation();
-	const iconUrl = "https://skillicons.dev/icons?i="+tech.name+"&theme=light";
+	const iconUrl = "https://skillicons.dev/icons?i="+tech.name+"&theme=dark";
 	
 	return (
 		<>
 			<div className="h-max items-center relative w-max" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
-				<div className={`bg-darkmode-bg rounded-md absolute duration-200 left-1/2 -translate-x-1/2 transition-opacity p-3 visible ease-in ${hover ? "translate-y-[-110%] opacity-100 visible" : "invisible opacity-0"}`}>
-					<span className={`${(tech.name == "aws" || tech.name == "gcp") ? "uppercase" : "capitalize"}`}>{tech.name}</span>
+				<div className={`bg-darkmode-bg rounded-md shadow-xl absolute duration-200 left-1/2 -translate-x-1/2 transition-opacity p-3 visible ease-in ${hover ? "translate-y-[-110%] opacity-100 visible" : "invisible opacity-0"}`}>
+					<span className={`${(tech.name == "aws" || tech.name == "gcp") ? "uppercase" : "capitalize"} text-zinc-200`}>{tech.name}</span>
 					<div className={`h-0 w-0 border-x-8 border-x-transparent duration-200 border-t-[16px] border-t-darkmode-bg absolute transition-opacity left-1/2 -translate-x-1/2 visible ease-in ${hover ? "translate-y-[30%] opacity-100 visible" : "invisible opacity-0"}`}/>
 				</div>
 				<div className="flex">
