4f52dce81648b9cac7dbb114f1ae2e0ed57ad49d
joaopedrosduarte <jpds@cesar.school>
2024-02-26 17:39:19 -0300

    Adjust header padding and background blur on hover on projects

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,7 +17,7 @@ export default function Header() {
   window.addEventListener("scroll", handleScroll);
 
   return (
-    <header className={`${ isHeaderMinimized ? "py-3" : "py-12 border-b-transparent" } border-b border-lightbase transition-all duration-500 bg-darkmode-bg w-full fixed z-50 flex justify-center align-middle backdrop-filter backdrop-blur-md bg-opacity-10`}>
+    <header className={`${ isHeaderMinimized ? "py-3" : "py-10 border-b-transparent" } border-b border-lightbase transition-all duration-500 bg-darkmode-bg w-full fixed z-50 flex justify-center align-middle backdrop-filter backdrop-blur-md bg-opacity-10`}>
       <nav className="flex justify-between max-w-5xl w-full px-10 items-center">
         <h1>
           <a href="/">Jpds.dev</a>
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -20,7 +20,7 @@ export default function Project({
       className="group flex flex-col bg-darkbase rounded-2xl w-full h-max justify-center items-center transition-all rg:max-w-none max-w-lg rg:flex-row"
     >
       <div className="absolute rg:max-w-[1034px] rg:h-[370px] h-[630px] max-w-[592px] w-full rg:w-full px-10 -z-10">
-        <div className="blur-md bg-darkbase opacity-0 group-hover:opacity-100 transition-opacity duration-300 h-full">
+        <div className="blur-xl bg-darkbase opacity-0 group-hover:opacity-100 transition-opacity duration-300 h-full">
           <div className={`${projectBackground} rg:h-[370px] h-[350px] max-w-[592px] rg:max-w-[525px] w-full rg:px-0`}/>
         </div>
       </div>
