cc48e4c7565256325722f9d425fd700f32406e4b
joaopedrosduarte <jpds@cesar.school>
2024-02-27 14:13:26 -0300

    add fonts and home component modularization

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,8 @@
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import Projects from "./components/Projects";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="m-auto max-[calc(100%-2rem)] text-white text-xl">
-      <Header />
-      <main className="w-full">
-        <Hero />
-        <About />
-        <Projects />
-        <Contact />
-      </main>
-      <Footer />
-    </div>
+    <Home />
   );
 }
 
