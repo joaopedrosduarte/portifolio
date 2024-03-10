62564f019cff3e21d85921871efbe8a7481e1eef
joaopedrosduarte <jpds@cesar.school>
2024-03-10 17:55:39 -0300

    Add color loader to avoid problems with loading

---
+++ src/components/ColorLoader.tsx
@@ -0,0 +1,9 @@
+export default function ColorLoader() {
+  return (
+    <>
+      <div className="bg-[#3498db] absolute"></div>
+      <div className="bg-[#2ecc71] absolute"></div>
+      <div className="bg-[#e74c3c] absolute"></div>
+    </>
+  )
+}
