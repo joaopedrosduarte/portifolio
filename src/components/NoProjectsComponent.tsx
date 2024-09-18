67c3bdfe30b530560c257ac7573175745aa07d07
joaopedrosduarte <jpds@cesar.school>
2024-09-18 14:45:39 -0300

    Adjust NoProjectsComponent style

--- src/components/NoProjectsComponent.tsx
+++ src/components/NoProjectsComponent.tsx
@@ -4,10 +4,10 @@ interface EmptyComponentProps {
 
 export default function NoProjectsComponent({ theme }: EmptyComponentProps) {
   return (
-    <div className="flex flex-col px-auto py-[120px] px-10 w-full items-center justify-between">
+    <div className="flex flex-col px-auto py-[60px] px-10 w-full items-center justify-between">
       <div className="gap-4 w-full flex flex-col items-center justify-between">
-        <span className={`${ theme == "light"? "text-zinc-700" : "text-darkmode-maintext" }  text-2xl font-bold block w-46 text-center`}>
-          Os projetos estão recebendo os seus toques finais e em breve estarão disponíveis! 😁
+        <span className={`${ theme == "light"? "text-lightmode-auxiliartext" : "text-darkmode-auxiliartext" }  text-xl font-bold block w-46 text-center`}>
+          Os projetos estão recebendo os seus toques finais e em breve estarão disponíveis! 💎
         </span>
       </div>
     </div>
